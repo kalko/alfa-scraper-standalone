02b1219318e16e551e208fac557d25ad8a56dda8
tamasnyulas-si <tny@siteimprove.com>
2025-02-06 08:07:28 +0100

    Introduced batch audit that sends results back to the CMS's ingest API

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,11 @@ import jwt from "jsonwebtoken"
 import { evaluatePageWithAlfa } from "./alfa/alfa-audit.js"
 import { validateUrl } from "./middleware.js"
 import { scrapePage } from "./scraper.js"
+import https from 'https'
+import url from 'url'
+
+// TODO: clean up authentication
+
 const authenticate = (req, res, next) => {
   const authHeader = req.headers["authorization"]
   const token = authHeader && authHeader.split(" ")[1]
@@ -20,7 +25,9 @@ const authenticate = (req, res, next) => {
     }
   )
 }
+
 const router = express.Router()
+
 router.post("/", validateUrl, async (req, res, next) => {
   try {
     const { targetUrl } = req.body
@@ -32,6 +39,7 @@ router.post("/", validateUrl, async (req, res, next) => {
     // res.status(500).json({ error: "Failed to process the scraping request." })
   }
 })
+
 router.post("/audit", validateUrl, async (req, res, next) => {
   try {
     const { targetUrl } = req.body
@@ -43,6 +51,7 @@ router.post("/audit", validateUrl, async (req, res, next) => {
     // res.status(500).json({ error: "Failed to process the scraping request." })
   }
 })
+
 router.post("/secured/scrape", authenticate, validateUrl, async (req, res) => {
   try {
     const { targetUrl } = req.body
@@ -53,6 +62,7 @@ router.post("/secured/scrape", authenticate, validateUrl, async (req, res) => {
     res.status(500).json({ error: "Failed to process the scraping request." })
   }
 })
+
 router.post("/secured/audit", authenticate, validateUrl, async (req, res) => {
   try {
     const { targetUrl } = req.body
@@ -63,4 +73,67 @@ router.post("/secured/audit", authenticate, validateUrl, async (req, res) => {
     res.status(500).json({ error: "Failed to process the scraping request." })
   }
 })
+
+// TODO: add request data validation
+router.post("/batch/audit", authenticate, async (req, res) => {
+  res.status(202).send();
+
+  console.log('Starting batch audit', req.body.targetUrls);
+
+  for (const targetUrl of req.body.targetUrls) {
+    try {
+      const auditData = await evaluatePageWithAlfa(targetUrl)
+      
+      const postData = JSON.stringify({
+        data: auditData,
+      });
+      const ingestUrl = new url.URL(req.body.ingestUrl)
+    
+      const token = jwt.sign({}, process.env.JWT_SECRET_KEY, {
+        expiresIn: '1h' 
+      });
+
+      const options = {
+        hostname: ingestUrl.hostname,
+        port: ingestUrl.port,
+        path: ingestUrl.pathname + ingestUrl.search,
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Content-Length': Buffer.byteLength(postData),
+          'Authorization': 'Bearer ' + token
+        },
+        rejectUnauthorized: false // TODO: use it only in local environment
+      };
+
+      // Create request to ingest API
+      const httpsReq = https.request(options, (res) => {
+        let responseData = '';
+        const protocol = res.socket.encrypted ? 'https' : 'http';
+        const host = res.socket.remoteAddress;
+        const port = res.socket.remotePort;
+        const path = res.req.path;
+
+        res.on('data', (chunk) => {
+          responseData += chunk;
+        });
+    
+        res.on('end', () => {
+            console.log(`POST ${protocol}://${host}:${port}${path} Response code: ${res.statusCode} ${res.statusMessage}`);
+            //console.log('Response body:', responseData);
+        });
+      });
+
+      httpsReq.on('error', (e) => {
+        console.error(`Problem with request: ${e.message}`, e);
+      });
+
+      httpsReq.write(postData);
+      httpsReq.end();
+    } catch (error) {
+      console.error("Scraping failed:", error)
+    }
+  }
+})
+
 export default router
